846dabeb998ca5078d3733f990b823e8c0a7e5f0
nel215 <otomo.yuhei@gmail.com>
2017-03-21 22:52:38 +0900

    Add a script using rdtsc

---
+++ asm.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+double ticks_per_sec = 3100000000;
+inline double get_time() {
+    uint32_t lo, hi;
+    asm volatile ("rdtsc" : "=a" (lo), "=d" (hi));
+    return (((uint64_t)hi << 32) | lo) / ticks_per_sec;
+}
+
+int main(){
+    for(int i=0; i<1000000000; i++){
+        get_time();
+    }
+    return 0;
+}
